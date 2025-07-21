134bb12a8f70e709938db9ba9c44dbc71b6585cd
Jagdevsinh <jagdevsinh1247@gmail.com>
2025-07-21 10:22:18 +0530

    Reverse number

---
+++ Reverse.js
@@ -0,0 +1,14 @@
+let num = 4562;
+
+    function reverseDigits(n) {
+        let revNum = 0;
+        while(n > 0)
+        {
+            revNum = revNum * 10 + n % 10;
+            n = Math.floor(n / 10);
+        }
+        return revNum;
+    }
+    
+ // function call   
+    console.log(reverseDigits(num));
