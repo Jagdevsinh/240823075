12e2986eea1529d8b0217cb786a4a10c767f4bac
Jagdevsinh <jagdevsinh1247@gmail.com>
2025-07-21 10:35:33 +0530

    Reverse

--- Reverse.js
+++ Reverse.js
@@ -3,7 +3,7 @@ let reversed=0;
 let number = num;
  while (number > 0){
      let digit = number %10;
-     reversed = reversed * 10;
+     reversed = reversed * 10 + digit;
      number = Math.floor(number/10);
  }
  console.log("Original number:",num);
