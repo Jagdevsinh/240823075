d079ef4931d0edc89c11dde87ac3928418dd1554
Jagdevsinh <jagdevsinh1247@gmail.com>
2025-08-06 13:30:11 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello")
+const express = require("express");
+const studentRoute = require("./routes/studentRoute"); 
+
+const app = express(); 
+const PORT = 80;
+
+app.use(express.json());
+
+app.use("/student", studentRoute);
+
+app.get("/" , (req, res) => {
+    res.send("hello how are you")
+})
+
+app.listen(PORT, () => {
+    console.log(`server is running at http://localhost:${PORT}/`);
+});
